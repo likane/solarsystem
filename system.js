7e351a4fc2bc984e5d5126df84f4c3c35c4d445b
Louis Kane <louiskane@1234.local>
2016-12-25 20:25:41 -0800

    add mercury orbit

--- system.js
+++ system.js
@@ -1,19 +1,79 @@
-var space = new Image();
+
+
+var sun = new Image();
+var moon = new Image();
 var earth = new Image();
+var mercury = new Image();
 
 function init(){
-	space.src = "assets/stars.jpg";
-	earth.src = "assets/Earth.png";
+  sun.src = 'assets/starsWeb.png';
+  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+  earth.src = 'assets/earthWeb1.png';
+  mercury.src = 'assets/mercury.png'
+  window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(mercury);
 }
 
 function draw() {
-	var ctx = document.getElementByID("canvas").getContext("2d");
+  var ctx = document.getElementById('canvas').getContext('2d');
 
-	ctx.globalCompositeOperation = "destination-over";
-	ctx.clearRect(0,0,1000,900);
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0,0,500,500); // clear canvas
 
-	ctx.fillStyle = "rgba(0,0,0,0.4)";
-	ctx.strokeStyle = "rgba(0,153,255,0.4)";
-	ctx.save();
-	ctx.translate(150,150);
+  ctx.fillStyle = 'rgba(0,0,0,0.4)';
+  ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+  ctx.save();
+  ctx.translate(150,150);
+
+  // Earth
+  var time = new Date();
+  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+  ctx.translate(105,0);
+  ctx.fillRect(0,-12,50,24); // Shadow
+  ctx.drawImage(earth,-12,-12);
+
+  // Moon
+  ctx.save();
+  ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+  ctx.translate(0,28.5);
+  ctx.drawImage(moon,-3.5,-3.5);
+  ctx.restore();
+
+  ctx.restore();
+  
+  ctx.beginPath();
+  ctx.arc(150,150,105,0,Math.PI*2,false); // Earth orbit
+  ctx.stroke();
+ 
+  ctx.drawImage(sun,0,0,300,300);
+
+  window.requestAnimationFrame(draw);
 }
+
+function mercury() {
+	var ctx = document.getElementById('canvas').getContext('2d');
+
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0,0,500,500); // clear canvas
+
+  ctx.fillStyle = 'rgba(0,0,0,0.4)';
+  ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+  ctx.save();
+  ctx.translate(50,50);
+
+  var time = new Date();
+  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+  ctx.translate(105,0);
+  //ctx.fillRect(0,-12,50,24); // Shadow
+  ctx.drawImage(mercury,-12,-12);
+
+  ctx.restore();
+  
+  ctx.beginPath();
+  ctx.arc(100,100,105,0,Math.PI*2,false); // Earth orbit
+  ctx.stroke();
+
+  window.requestAnimationFrame(mercury);
+}
+
+init();
