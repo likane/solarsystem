53b40f47acaa28a644143ccacefb2ffbb36c8caf
Louis Kane <louiskane@1234.local>
2016-12-27 00:04:41 -0800

    mercury now orbits the sun

--- system.js
+++ system.js
@@ -11,7 +11,7 @@ function init(){
   earth.src = 'assets/earthWeb1.png';
   mercury.src = 'assets/mercury.png'
   window.requestAnimationFrame(draw);
-  window.requestAnimationFrame(mercury);
+  //window.requestAnimationFrame(mercury);
 }
 
 function draw() {
@@ -25,24 +25,34 @@ function draw() {
   ctx.save();
   ctx.translate(150,150);
 
+ //mercury
+   ctx.save();
+   var mercTime = new Date();
+   ctx.rotate(((2*Math.PI)/60)*mercTime.getSeconds() + ((2*Math.PI)/60000)*mercTime.getMilliseconds());
+   ctx.translate(20,0);
+   ctx.drawImage(mercury,-9,-9);
+   ctx.restore();
+
   // Earth
   var time = new Date();
-  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+  ctx.rotate(((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds());
   ctx.translate(105,0);
   ctx.fillRect(0,-12,50,24); // Shadow
   ctx.drawImage(earth,-12,-12);
 
   // Moon
   ctx.save();
-  ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+  ctx.rotate(((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds());
   ctx.translate(0,28.5);
   ctx.drawImage(moon,-3.5,-3.5);
   ctx.restore();
+
 
   ctx.restore();
   
   ctx.beginPath();
   ctx.arc(150,150,105,0,Math.PI*2,false); // Earth orbit
+  //ctx.arc(50,50,105,0, Math.PI*2, false);
   ctx.stroke();
  
   ctx.drawImage(sun,0,0,300,300);
@@ -50,30 +60,5 @@ function draw() {
   window.requestAnimationFrame(draw);
 }
 
-function mercury() {
-	var ctx = document.getElementById('canvas').getContext('2d');
-
-  ctx.globalCompositeOperation = 'destination-over';
-  ctx.clearRect(0,0,500,500); // clear canvas
-
-  ctx.fillStyle = 'rgba(0,0,0,0.4)';
-  ctx.strokeStyle = 'rgba(0,153,255,0.4)';
-  ctx.save();
-  ctx.translate(50,50);
-
-  var time = new Date();
-  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
-  ctx.translate(105,0);
-  //ctx.fillRect(0,-12,50,24); // Shadow
-  ctx.drawImage(mercury,-12,-12);
-
-  ctx.restore();
-  
-  ctx.beginPath();
-  ctx.arc(100,100,105,0,Math.PI*2,false); // Earth orbit
-  ctx.stroke();
-
-  window.requestAnimationFrame(mercury);
-}
 
 init();
